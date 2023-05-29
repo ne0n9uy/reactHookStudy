b4b9e62c09712e1cc8809522dd6ee0ea5381bb16
ne0n9uy <2leebins@naver.com>
2023-05-29 23:43:10 +0900

    reactHookStudy01

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Counter from './Counter';
-import Info from './Info';
+import Counter from './useState01_Counter';
+import Info from './useState02_Info';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
