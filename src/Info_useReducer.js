18827844e003e4b0ba730d483703fe4ce94e018a
ne0n9uy <2leebins@naver.com>
2023-06-04 23:46:41 +0900

    useReducer

--- src/Info_useReducer.js
+++ src/Info_useReducer.js
@@ -1,33 +1,35 @@
 import React,{useReducer} from "react";
 
-    function reducer(state,action){
-        //1.사용할 함수 작성
-        return(
-            {
-                ...state,
-                [action.name]:action.value
-            }
-        );
-    };
+//1. 사용할 함수 정의
+function reducer(state, action){ //state, action
+    return{ //여러 값 관리
+            ...state,
+            [action.name]:action.value
+        };
+}
 
-    const Info_useReducer=()=>{
-        const [state,dispatch]=useReducer(reducer,
-            { //2.useReducer 통해 사용 함수, 초기값 지정
-                snack:'',
-                reason:''
-            });
-
-        const {snack,reason}=state; //3.state에 변수 추가
-        const onChange=(e)=>{
-            dispatch(e.target); //4.dispatch 통해 reducer 함수 호출하는 파라미터 지정
-        }
-        return(
-            <>
-                <input name="snack" value={snack} onChange={onChange}></input>
-                <input name="reason" value={reason} onChange={onChange}></input>
-                <p>좋아하는 과자: {snack}</p>
-                <p>좋아하는 이유: {reason}</p>
-            </>
+//2. main 
+const Info_useReducer=()=>{
+    const [state,dispatch]=useReducer(reducer,{ 
+        //useReducer 사용 시 state값, dispatch 함수 받아옴
+        //state:현 상태, dispatch: action 발생시킴
+        snack:'',
+        reason:''
+    });
+    
+    const {snack,reason}=state; //state 할당
+    const onChange=(e)=>{
+        //action 값 넣어주면 reducer 함수 호출 구조
+       dispatch(e.target); //action은 어떤 값도 가능
+    }
+    
+    return(
+        <>
+            <input name="snack" value={snack} onChange={onChange}></input>
+            <input name="reason" value={reason} onChange={onChange}></input>
+            <p>좋아하는 과자:{snack}</p>
+            <p>좋아하는 이유:{reason}</p>
+        </>
         );
     }
 
